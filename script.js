91e46db06253d723c37f161095a3f68608e51d4e
Tabata <tabatac.souto@gmail.com>
2021-12-22 12:15:21 -0300

    Tentando descobrir o que esta errado

--- script.js
+++
@@ -1,4 +0,0 @@
-const age = document.querySelector('.idade');
-const now = new Date;
-
-age.innerText = now.getFullYear() - 1992;
---
+++ script/script.js
@@ -0,0 +1,4 @@
+const age = document.querySelector('.idade');
+const now = new Date;
+
+age.innerText = now.getFullYear() - 1992;
